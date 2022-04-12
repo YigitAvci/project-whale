8621be9736dddb39a1b187f630d861ca65cc760f
Yiğit Avcı <yigit.avci@solviads.com>
2022-04-12 19:04:11 +0300

    implements user delete method

--- src/main/java/com/sloths/movie_review_project/auth/controllers/UserController.java
+++ src/main/java/com/sloths/movie_review_project/auth/controllers/UserController.java
@@ -4,6 +4,7 @@ import com.sloths.movie_review_project.auth.entities.User;
 import com.sloths.movie_review_project.auth.services.UserService;
 import com.sloths.movie_review_project.helpers.responseHelpers.CustomResponseEntity;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,7 +22,12 @@ public class UserController {
     }
 
     @PostMapping("/addUser")
-    public CustomResponseEntity add(@RequestBody User user) {
+    public CustomResponseEntity<User> add(@RequestBody User user) {
         return userManager.add(user);
+    }
+
+    @DeleteMapping("/deleteUser")
+    public CustomResponseEntity<User> delete(@RequestBody User user) {
+        return userManager.delete(user);
     }
 }
--- src/main/java/com/sloths/movie_review_project/auth/services/UserService.java
+++ src/main/java/com/sloths/movie_review_project/auth/services/UserService.java
@@ -6,6 +6,7 @@ import com.sloths.movie_review_project.helpers.responseHelpers.CustomResponseEnt
 import com.sloths.movie_review_project.helpers.responseHelpers.CustomResponseEntityFail;
 import com.sloths.movie_review_project.helpers.responseHelpers.CustomResponseEntitySuccess;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -22,10 +23,37 @@ public class UserService {
     }
 
     public CustomResponseEntity add(User user) {
-        if(user != null) {
-            user.setPassword(passwordEncoder.encode(user.getPassword()));
-            return new CustomResponseEntitySuccess(userDataAccess.save(user), "user has been saved, successfully!");
+        if(user == null || !isUsernameProper(user.getUsername()) || !isPasswordProper(user.getPassword())) {
+            return new CustomResponseEntityFail("an error occurred during storing the user! try again, please...");
         }
-        return new CustomResponseEntityFail("an error occurred during storing the user! try again, please...");
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        return new CustomResponseEntitySuccess<>(userDataAccess.save(user), "user has been saved, successfully!");
+    }
+
+    public CustomResponseEntity<User> delete(User user) {
+        if(user == null) {
+            return new CustomResponseEntityFail("an error occurred during deleting the user! try again, please...");
+        }
+        User userToBeDeleted = userDataAccess.getById(user.getId());
+        try {
+            userDataAccess.delete(user);
+        }catch (DataAccessException dataAccessException) {
+            return new CustomResponseEntityFail("a database access exception has been occurred!");
+        }
+        return new CustomResponseEntitySuccess(userToBeDeleted, "user has been deleted, successfully!");
+    }
+
+    private boolean isPasswordProper(String password) {
+        if(password.length() < 8 || !password.matches("(.*)[a-zA-Z](.*)") || !password.matches("(.*)[0-9](.*)")) {
+            return false;
+        }
+        return true;
+    }
+
+    private boolean isUsernameProper(String username) {
+        if(username.length() == 0 || username.contains(" ")) {
+            return false;
+        }
+        return true;
     }
 }
